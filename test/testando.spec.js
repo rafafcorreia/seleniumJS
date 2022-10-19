d198b071a1e0cd5dd1333c2188c110dc40781d70
rafafcorreia <rafa.f.correia@gmail.com>
2022-10-19 19:54:12 -0300

    Atualizando os testes

--- test/testando.spec.js
+++ test/testando.spec.js
@@ -1,48 +1,52 @@
 const webdriver = require('selenium-webdriver');
 const By = webdriver.By;
 const assert = require('chai').assert;
+require('chromedriver');
 
 describe('Google', () => {
-  let driver;
+    let driver;
 
-  beforeEach(() => {
-    driver = new webdriver.Builder()
-      .forBrowser('chrome')
-      .build()
-  })
+    beforeEach(() => {
+        driver = new webdriver.Builder()
+        .forBrowser('chrome')
+        .build()
+    })
 
-  afterEach(() => {
-    driver.quit();
-  })
+    afterEach(() => {
+        driver.quit();
+    })
     
-  it('Pesquisar webdriver - Com clique', done => {
-    driver.get('https://www.google.com')
-    .then(() => driver.findElement(By.name('q')).sendKeys('webdriver'))
-    .then(() => driver.sleep(5000))
-    .then(() => driver.findElement(By.name('btnK')).click())
-    // .then(() => driver.findElement(By.name('q')).sendKeys(webdriver.Key.ENTER))
-    .then(() => driver.sleep(5000))
-    .then(() => driver.getTitle())
-    .then(title => {
-      assert.equal(title, 'webdriver - Pesquisa Google');
+    it('Pesquisar webdriver - Com clique', async() => {
+        let tituloWebDriver;
+        let cabecalho;
+        
+        await driver.get('https://www.google.com')
+        await driver.findElement(By.name('q')).sendKeys('webdriver')
+        await driver.sleep(5000)
+        await driver.findElement(By.name('btnK')).click()
+        // await driver.findElement(By.name('q')).sendKeys(webdriver.Key.ENTER)
+        await driver.sleep(5000)
+        let title = await driver.getTitle()
+        assert.equal(title, 'webdriver - Pesquisa Google');
+        
+        await driver.findElement(By.css('a[href^="https://chromedriver.chromium.org/"]')).click()
+        await driver.sleep(5000)
+        tituloWebDriver = await driver.getTitle();
+        cabecalho = await driver.findElement(By.css('span.Rn3Z1b')).getText();
+        assert.equal(tituloWebDriver, 'ChromeDriver - WebDriver for Chrome');
+        assert.equal(cabecalho, 'ChromeDriver');
     })
-    .then(done)
-    .catch(err => done(err));
-  })
 
-  it('Pesquisar Iterasys - Com Enter', done => {
-    driver.get('https://www.google.com')
-    .then(() => driver.findElement(By.name('q')).sendKeys('Iterasys'))
-    .then(() => driver.sleep(5000))
-    .then(() => driver.findElement(By.name('q')).sendKeys(webdriver.Key.ENTER))
-    .then(() => driver.sleep(5000))
-    .then(() => driver.getTitle())
-    .then(title => {
-      assert.equal(title, 'Iterasys - Pesquisa Google');
+    it('Pesquisar Iterasys - Com Enter', async() => {
+        await driver.get('https://www.google.com');
+
+        await driver.findElement(By.name('q')).sendKeys('Iterasys')
+        await driver.sleep(5000)
+        await driver.findElement(By.name('q')).sendKeys(webdriver.Key.ENTER)
+        await driver.sleep(5000)
+        let title = await driver.getTitle()
+        assert.equal(title, 'Iterasys - Pesquisa Google');
     })
-    .then(done)
-    .catch(err => done(err));
-  })
 })
 
 
